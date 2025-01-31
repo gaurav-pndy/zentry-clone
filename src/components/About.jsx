946f4d24bbecd4143344880f48570c28fd21439c
gaurav-pndy <gauravpndy00@gmail.com>
2025-02-01 04:17:19 +0530

    final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
     const clipAnimation = gsap.timeline({
       scrollTrigger: {
         trigger: "#clip",
-        start: "center center",
+        start: "top center",
         end: "+=800 center",
         scrub: 0.5,
         pin: true,
